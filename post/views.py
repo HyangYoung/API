d205e218677393869942b62dc4f104e99594e4f9
HyangYoung <hyangyoungs@gmail.com>
2022-04-05 00:27:50 -0500

    add 'post' model

--- post/views.py
+++ post/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request.body", request.body) # logger recommend
+    #     print("request.POST", request.POST) # logger recommend
+    #     return super().dispatch(request, *args, **kwargs)
+
+
+# def post_list(request):
+#     pass
+#
+# def post_detail(request, pk):
+#     pass
